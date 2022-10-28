432c37baa190d3d21ded1f0d5224e444f976d13c
Felix Angelico Sakuayela <f.sakuayela@gmail.com>
2022-10-28 03:52:17 -0300

    Create aula1.js
    commit desafio

---
+++ aula1.js
@@ -0,0 +1,8 @@
+/* Aula 1
+Faça um programa para colocar o valor de uma viagem
+    Vc terá 3 variáveis
+        1 - Preço do combustível (precoCombustivel)
+        2 - Gasto médio do combustível gasto do carro por KM (distanciaEmKm)
+        3 - Distância em KM da viagem (distanciaEmKm)
+
+Imprima no console o valor gasto de combustível para realizar esta viagem */
